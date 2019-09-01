a56bd93fa23eb7dbd1c6028aead07730fdef2ab0
reaganysmith <reaganysmith@192.168.1.16>
2019-09-01 17:49:18 -0400

    Working copy without final comments.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,34 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read in and store the number of customers - i.e. the first integer
+		int customerCount = scan.nextInt();
+		
+		// Create two string arrays: one to store the first name, and one to store the last.
+		String[] fName = new String[customerCount];
+		String[] lName = new String[customerCount];
+		
+		double[] output = new double[customerCount];
+		
+		for (int i = 0; i < customerCount; i++) {
+			fName[i] = scan.next();
+			lName[i] = scan.next();
+			
+			int totalBought = scan.nextInt();
+			
+			for (int a = 0; a < totalBought; a++) {
+				int iTemp = scan.nextInt();
+				double dTemp = scan.nextDouble();
+				
+				output[i] += iTemp * dTemp;
+				
+			}
+			
+		}
+		
+		for (int i = 0; i < customerCount; i++) {
+			System.out.println(fName[i].charAt(0) + ". " + lName[i] + ": " + String.format("%.2f", output[i]));
+		}
 		
 	}
 }
